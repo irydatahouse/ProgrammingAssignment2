ba282021ccafcbfc0639781a9167259c9d4e86d5
Iryna <iryhouse@gmail.com>
2014-11-20 20:07:27 +0200

    finished task assgnmt 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  These are R functions are able to cache
+##  potentially time-consuming computations.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  checkinverse <- function(){
+    if (is.null(inv)) {
+      message("can't check inverse  - inverted matrix is NULL")      
+      return()
+    }
+    
+    inv %*% x
+  }
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse,
+       checkinverse = checkinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix. 
+## If the inverse has already been calculated
+## (and the matrix has not changed), then the cachesolve
+##  should retrieve the inverse from the cache. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data (inverted matrix)")
+    return(inv)
+  }
+  data <- x$get()
+  nc <- ncol(data)
+  nr <- nrow(data)
+
+  if(nc != nr) {
+    message("non-square matrix - inverse is NULL")
+  } else {
+    inv <- try(solve(data,...))
+  }
+  
+  x$setinverse(inv)
+  
+  inv
+
 }
